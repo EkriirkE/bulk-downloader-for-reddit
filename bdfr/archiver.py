949538ea431d6e26a5db7dc2f89b0d6823711ca8
Erik Johnson <source@ekriirke.net>
2026-04-06 21:42:25 +0200

    Dirty retry loops, needs work

--- bdfr/archiver.py
+++ bdfr/archiver.py
@@ -30,28 +30,34 @@ class Archiver(RedditConnector):
 
     def download(self) -> None:
         for generator in self.reddit_lists:
-            try:
-                for submission in generator:
-                    try:
-                        if (submission.author and submission.author.name in self.args.ignore_user) or (
-                            submission.author is None and "DELETED" in self.args.ignore_user
-                        ):
-                            logger.debug(
-                                f"Submission {submission.id} in {submission.subreddit.display_name} skipped due to"
-                                f" {submission.author.name if submission.author else 'DELETED'} being an ignored user"
-                            )
-                            continue
-                        if submission.id in self.excluded_submission_ids:
-                            logger.debug(f"Object {submission.id} in exclusion list, skipping")
-                            continue
-                        logger.debug(f"Attempting to archive submission {submission.id}")
-                        self.write_entry(submission)
-                    except prawcore.PrawcoreException as e:
-                        logger.error(f"Submission {submission.id} failed to be archived due to a PRAW exception: {e}")
-            except prawcore.PrawcoreException as e:
-                logger.error(f"The submission after {submission.id} failed to download due to a PRAW exception: {e}")
-                logger.debug("Waiting 60 seconds to continue")
-                sleep(60)
+            for retryb in range(5):
+                try:
+                    for submission in generator:
+                        for retry in range(5):
+                            try:
+                                if (submission.author and submission.author.name in self.args.ignore_user) or (
+                                    submission.author is None and "DELETED" in self.args.ignore_user
+                                ):
+                                    logger.debug(
+                                        f"Submission {submission.id} in {submission.subreddit.display_name} skipped due to"
+                                        f" {submission.author.name if submission.author else 'DELETED'} being an ignored user"
+                                    )
+                                    break
+                                if submission.id in self.excluded_submission_ids:
+                                    logger.debug(f"Object {submission.id} in exclusion list, skipping")
+                                    break
+                                logger.debug(f"Attempting to archive submission {submission.id}")
+                                self.write_entry(submission)
+                                break
+                            except prawcore.PrawcoreException as e:
+                                logger.error(f"Submission {submission.id} failed to be archived due to a PRAW exception: {e}")
+                                logger.debug("Waiting 60 seconds to continue")
+                                sleep(60)
+                    break
+                except prawcore.PrawcoreException as e:
+                    logger.error(f"The submission after {submission.id} failed to download due to a PRAW exception: {e}")
+                    logger.debug("Waiting 60 seconds to continue")
+                    sleep(60)
 
     def get_submissions_from_link(self) -> list[list[praw.models.Submission]]:
         supplied_submissions = []
--- bdfr/cloner.py
+++ bdfr/cloner.py
@@ -19,14 +19,20 @@ class RedditCloner(RedditDownloader, Archiver):
 
     def download(self) -> None:
         for generator in self.reddit_lists:
-            try:
-                for submission in generator:
-                    try:
-                        self._download_submission(submission)
-                        self.write_entry(submission)
-                    except prawcore.PrawcoreException as e:
-                        logger.error(f"Submission {submission.id} failed to be cloned due to a PRAW exception: {e}")
-            except prawcore.PrawcoreException as e:
-                logger.error(f"The submission after {submission.id} failed to download due to a PRAW exception: {e}")
-                logger.debug("Waiting 60 seconds to continue")
-                sleep(60)
+            for retryb in range(5):
+                try:
+                    for submission in generator:
+                        for retry in range(5):
+                            try:
+                                self._download_submission(submission)
+                                self.write_entry(submission)
+                                break
+                            except prawcore.PrawcoreException as e:
+                                logger.error(f"Submission {submission.id} failed to be cloned due to a PRAW exception: {e}")
+                                logger.debug("Waiting 60 seconds to continue")
+                                sleep(60)
+                    break
+                except prawcore.PrawcoreException as e:
+                    logger.error(f"The submission after {submission.id} failed to download due to a PRAW exception: {e}")
+                    logger.debug("Waiting 60 seconds to continue")
+                    sleep(60)
